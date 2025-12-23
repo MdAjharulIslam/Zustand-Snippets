558a1051ce54d6c0c0791bc6572fcf63ae6ae9a2
ajharul <mdajharulislam327@gmail.com>
2025-12-24 00:49:58 +0600

    added sex option

--- src/store/UserStore.tsx
+++ src/store/UserStore.tsx
@@ -9,16 +9,20 @@ interface UserStore {
   setName: (newName: string) => void;
   setAge: (newAge: number) => void;
   resetUser: () => void;
+  sex: string;
+  setSex:(newSex:string)=> void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   name: "Ajharul",
   age: 18,
   count: 0,
+  sex:"male",
   setCount: () => set((state) => ({ count: state.count + 1 })),
   setName: (newName) => set({ name: newName }),
   setAge: (newAge) => set({ age: newAge }),
   resetUser: () => set({ name: "Ajharul", age: 18,count:0 }),
+  setSex: (newSex)=> set({sex:newSex})
 }));
 
 export const userStore = create((set)=>({
